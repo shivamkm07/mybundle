be9b8d2988f0112cb88afe79b79eed2e6389460c
shivam <shivamkm07@gmail.com>
2022-03-07 13:54:46 +0530

    Fixing failures

--- .github/scripts/build_daprbundle.py
+++ .github/scripts/build_daprbundle.py
@@ -1,6 +1,21 @@
+# ------------------------------------------------------------
+# Copyright 2021 The Dapr Authors
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ------------------------------------------------------------
+
+
 import argparse
 from errno import ESTALE
 from fileinput import filename
+from http.client import OK
 import subprocess
 import tarfile
 import zipfile
@@ -9,7 +24,6 @@ import json
 import os
 import sys
 import shutil
-
 
 
 # GitHub Organization and repo name to download release
@@ -36,8 +50,12 @@ def getLatestRelease(repo):
     daprReleaseUrl = "https://api.github.com/repos/" + GITHUB_ORG + "/" + repo + "/releases"
     print(daprReleaseUrl)
     latest_release = ""
-    resp = requests.get(daprReleaseUrl).text
-    data = json.loads(resp)
+    resp = requests.get(daprReleaseUrl)
+    if resp.status_code != requests.codes.ok:
+        print(f"Error pulling latest release of {repo}")
+        resp.raise_for_status()
+        sys.exit(1)
+    data = json.loads(resp.text)
     version = data[0]['tag_name'].lstrip("v")
     print(version)
     return version
